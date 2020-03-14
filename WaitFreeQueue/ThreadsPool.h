1b66f6c819fa517df641e9525b2701d79dff4414
Andrea Graziani <andrea.graziani93@outlook.it>
2020-03-14 09:33:05 +0100

    ....

--- WaitFreeQueue/ThreadsPool.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-pthread_t *startThreadPool(unsigned long long poolSize, void *(*thread_routine)(void *), void *threadInputData);
-
-void joinAndFreeThreadsPool(pthread_t *threadPoll, unsigned long long poolSize);
